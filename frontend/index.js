2f2ed6e6ddaa032e4584f23f25baa062189604d6
Aarni Koskela <akx@iki.fi>
2018-07-26 19:36:02 +0300

    Rework frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,54 +1,91 @@
 import m from 'mithril';
 import './style.css';
+import logoSvg from '../assets/logo.svg';
 
 function post() {
-  const form = this;
   fetch('/', {
     method: 'POST',
-    body: new FormData(form)
+    body: JSON.stringify({
+      language: state.language,
+      content: state.content,
+    }),
+    headers: {
+      'content-type': 'application/json',
+    }
   })
     .then(resp => resp.json())
     .then(data => {
-      const out = document.getElementById('output');
-      out.classList.toggle('error', !!data.error);
-      out.classList.toggle('success', !!data.content);
-      document.getElementById('code').innerText = data.content || '';
-      delete data.content;
-      document.getElementById('message').innerText = data.error || JSON.stringify(data);
+      state.result = data;
+      m.redraw();
     });
-  return false;
 }
 
-const content = `
+const state = {
+  language: 'python',
+  content: `
 movies = {"title":["Mission: Impossible - Fallout"], "genres":["Action", "Adventure", "Thriller"], 'storyline':[' \\n  Ethan Hunt and his IMF team, along with some familiar allies, race against time after a mission gone wrong.    \\n ']}
 print(dict((k, list(map(str.strip, v))) for k,v in movies.items()))
-`;
+`,
+  result: null,
+};
 
-const view = () => (
-  <main>
-    <h1>Nicen</h1>
-    <a href="/openapi.yaml">OpenAPI Spec</a>
-    <table width="100%">
-      <tr>
-        <td>
-          <form method="post" onsubmit={post}>
-            <select name="language">
-              <option value="python">Python</option>
-              <option value="c">C</option>
-              <option value="javascript">JavaScript</option>
-            </select>
-            <textarea name="content">{content}</textarea>
-            <input type="submit" value="nicen" />
-          </form>
-        </td>
-        <td id="output">
-          <div id="message" />
-          <div id="code" />
-        </td>
-      </tr>
-    </table>
-  </main>
+const outputView = () => {
+  const className = [];
+  const { result } = state;
+  let message;
+  if (result) {
+    if (result.error) {
+      className.push('error');
+    }
+    if (result.content) {
+      className.push('success');
+    }
+    const mData = { ...result };
+    delete mData.content;
+    message = mData.error || JSON.stringify(mData, null, 1).replace(/\n/g, ' ');
+  }
+  return (
+    <section id="output" class={className.join(' ')}>
+      <div id="message">{message}</div>
+      <div id="code">{result && result.content ? result.content : null}</div>
+    </section>
+  );
+};
+
+const inputView = () => (
+  <form id="input" method="post" onsubmit={(event) => {
+    post();
+    event.preventDefault();
+    return false;
+  }}>
+    <select name="language" value={state.language} onchange={(event) => {
+      state.language = event.target.value;
+    }}>
+      <option value="python">Python</option>
+      <option value="c">C</option>
+      <option value="javascript">JavaScript</option>
+    </select>
+    <textarea name="content" oninput={(event) => {
+      state.content = event.target.value;
+    }}>{state.content}</textarea>
+    <button type="submit">Nicen my code!</button>
+  </form>
 );
+
+const view = () => [
+  <nav>
+    <img src={logoSvg} alt="Nicen" />
+  </nav>,
+  <main>
+    {inputView()}
+    {state.result ? outputView() : null}
+  </main>,
+  <footer>
+    An <a href="https://akx.github.io/">@akx</a> joint.
+    &middot;&nbsp;<a href="/openapi.yaml">OpenAPI Spec</a>
+    &nbsp;&middot;&nbsp;<a href="https://github.com/akx/nicen">Source code</a>
+  </footer>,
+];
 
 
 m.mount(document.body, { view });
--- frontend/poi.config.js
+++ frontend/poi.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  html: {
+    title: 'Nicen'
+  },
   devServer: {
     proxy: {
       '/': 'http://localhost:8042'
