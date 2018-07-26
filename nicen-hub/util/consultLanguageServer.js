491ae4051ad25e30013ca61e4cddac9794992a3e
Aarni Koskela <akx@iki.fi>
2018-07-26 18:20:13 +0300

    Embetter error handling

--- nicen-hub/util/consultLanguageServer.js
+++ nicen-hub/util/consultLanguageServer.js
@@ -1,16 +1,24 @@
 const request = require('request-promise-native');
 
-const consultLanguageServer = async function(url, content, {width}) {
-  const resp = await request({
-    method: 'post',
-    uri: url,
-    qs: {width},
-    body: content,
-    headers: {
-      'Content-type': 'text/plain',
-    },
-    resolveWithFullResponse: true,
-  });
+const consultLanguageServer = async function (url, content, { width }) {
+  let resp;
+  try {
+    resp = await request({
+      method: 'post',
+      uri: url,
+      qs: { width },
+      body: content,
+      headers: {
+        'Content-type': 'text/plain',
+      },
+      resolveWithFullResponse: true,
+      simple: false,
+    });
+  } catch (err) {
+    return {
+      error: `Unable to consult language server: ${err}`,
+    };
+  }
   if (resp.statusCode === 200) {
     return {
       content: resp.body,
