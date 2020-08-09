4088923ca2c9e6532f5d5a6fc7f7541b7939937f
Aarni Koskela <akx@iki.fi>
2020-08-09 10:05:00 +0300

    Add basic Carbon

--- frontend/index.js
+++ frontend/index.js
@@ -4,3 +4,10 @@ import App from './App';
 import './style.css';
 
 ReactDOM.render(React.createElement(App), document.getElementById('app'));
+
+// eslint-disable-next-line no-restricted-globals
+if (process.env.NODE_ENV === 'development' || location.hostname.includes('nicen.pw')) {
+  import('./carbon').then((carbonModule) => {
+    carbonModule.injectCarbonTag();
+  });
+}
